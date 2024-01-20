d331989bb767c57301741f17df7db2bdc585dbcf
Kirill Shamanin <elovgar4@gmail.com>
2024-01-20 20:10:14 +0300

    Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arr2 = new string[arr1.Length];
+
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}   
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+SecondArray(arr1, arr2);
+PrintArray(arr2);
